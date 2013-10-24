b348405e409d210e7fd3f825cea0ce3ba5d342fe
michel94 <miguel.duarte20@hotmail.com>
2013-10-24 09:28:53 +0000

    Added movement with mouse, only experimental

--- src/Selection.cpp
+++ src/Selection.cpp
@@ -1,15 +1,38 @@
 #include "Selection.hpp"
 
 bool checkClick(Engine *eng, Drawable *d, int x, int y){
-	//float ix = eng->get_iso_x(d->x, d->y);
-	//float iy = eng->get_iso_y(d->x, d->y);
 	float ix = d->x;
 	float iy = d->y;
-	//printf("%f %f %d %d %d %d\n", ix, iy, x, y, d->width, d->height);
 	if(x >= ix && y >= iy && x <= ix + d->width && y <= iy + d->height){
-		//printf("inside\n");
 		return Wrapper::get_pixel(d->surface, x - ix, y - iy);
 	}
 	return false;
 }
 
+bool checkClickonEngine(Engine *eng, Drawable *obj, int x, int y){
+	Drawable *col = new Drawable(obj->surface, obj->width, obj->height);
+	col->x = eng->get_iso_x(obj->x, obj->y);
+	col->y = eng->get_iso_y(obj->x, obj->y) + 18;
+	//Wrapper::draw_image(col->surface, 1 * col->x, 1 * col->y, col->width, col->height);
+	return checkClick(eng, col, x, y);
+}
+
+
+Drawable* setMovementTarget(Engine* engine){
+	int x, y;
+	SDL_GetMouseState(&x, &y);
+	for (int i = 0; i < engine->vert_number; i++) {
+		for (int j = 0; j < engine->horiz_number; j++) {
+			Drawable *d;
+			d = new Drawable(engine->get_tile_surface(i, j), engine->width, 36);
+			d->x = i;
+			d->y = j;
+			if(checkClickonEngine(engine, d, x, y)){
+				if(SDL_GetMouseState(&x, &y)&SDL_BUTTON(3)){
+					return d;
+				}
+			}
+		}
+	}
+	return NULL;
+}
--- src/Selection.hpp
+++ src/Selection.hpp
@@ -10,5 +10,7 @@ using namespace sdl_wrapper;
 using namespace isometric_engine;
 
 bool checkClick(Engine *eng, Drawable *d, int x, int y);
+bool checkClickonEngine(Engine *eng, Drawable *obj, int x, int y);
+Drawable* setMovementTarget(Engine* engine);
 
 #endif
